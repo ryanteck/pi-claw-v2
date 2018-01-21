700f91ffe52e2db4dc92c41acfceb5e35287bad9
Ryan walmsley <ryanteck@Gmail.com>
2018-01-21 13:26:13 +0000

    Modifications including: Improving return to home sequence, Created function for dropping the claw, picking up and lifting.

--- clawLib.py
+++ clawLib.py
@@ -18,14 +18,32 @@ rBut = Button(13)
 #Define Magnet
 clawMagnet = PWMLED(18,frequency=100)
 def returnToHome():
-        lrStop.when_pressed = lrM.stop
-        fbStop.when_pressed = fbM.stop
-        lrM.backward()
-        fbM.backward()
-        while(lrStop.value==False or fbStop.value==False):
-            pass
-        lrStop.when_pressed = None
-        fbStop.when_pressed = None
+    lrStop.when_pressed = lrM.stop
+    fbStop.when_pressed = fbM.stop
+    lrM.backward()
+    fbM.backward()
+    while(lrStop.value==False or fbStop.value==False):
+        pass
+    lrM.stop() #Sometimes even though this should have been run it doesn't.
+    fbM.stop()
+    lrStop.when_pressed = None
+    fbStop.when_pressed = None
+    clawMagnet.off()
+
+def grabProcedure(strength):
+    #Procedure for grabbing the item.
+    udM.backward()
+    cT = 0
+    while(cT<16 and dStop.value==False):
+        print(cT)
+        sleep(1)
+        cT = cT+1
+    udM.stop()
+    #grab
+    clawMagnet.value = strength
+    udM.forward()
+    uStop.wait_for_press()
+    udM.stop()
 
 def initProcedure():
     #First move the motor one second forward and right & Drop claw 2 secs
@@ -55,5 +73,10 @@ def initProcedure():
     print("True") #Both switches are hit by now and the when_pressed function should have ran but doesn't seem to trigger for 5 seconds.
 
 
-
-initProcedure()
+fbM.forward()
+lrM.forward()
+sleep(2)
+fbM.stop()
+lrM.stop()
+grabProcedure(0.5)
+returnToHome()
